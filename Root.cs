a69941bc05a48d15184cb18bdf2ad22a320fd368
ThomSolo <113390367+ThomSolo@users.noreply.github.com>
2023-02-09 23:20:58 -0500

    Add files via upload
    Error Pushing files due to 400+ files. Unkown where they have gone. Followed given instructions.

---
+++ Root.cs
@@ -0,0 +1,17 @@
+﻿//Solomon, Etikayo, and Riley
+namespace MyRootLib;
+
+ public class Root {
+        /// <summary>
+        /// this method calculates the root
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static double MyRoot(double number) {
+            double root;
+            root = Math.Sqrt(number);
+
+            Console.WriteLine("The square root of " + number + " is " + root);
+            return root;
+        }
+    }
